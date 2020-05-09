d8cfa72a58e91d7f509f7bdf9201133e2ebd071b
swiftcoder.gp <P@ndav91@GP>
2020-05-09 16:25:13 +0530

    Updated with react-router-dom and withRouter.

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -46,9 +46,9 @@ class Directory extends Component {
     render() {
         return (
             <div className='directory-menu'>
-                {this.state.sections.map(({id, title, imageUrl, size}) => {
+                {this.state.sections.map(({id, ...otherSectionProps}) => {
                     return (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                        <MenuItem key={id} {...otherSectionProps}/>
                     )
                 })}
             </div>
