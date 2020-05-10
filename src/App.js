21a30ca70a2620649e08bb8fe5d4ef881ee9ff3f
swiftcoder.gp <P@ndav91@GP>
2020-05-10 16:01:56 +0530

    Added header.component.jsx and links.

--- src/App.js
+++ src/App.js
@@ -3,10 +3,12 @@ import './App.css';
 import Homepage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shopPage/shopPage.component";
 import {Route} from 'react-router-dom';
+import Header from "./components/header/header.component";
 
 function App() {
     return (
         <div>
+            <Header/>
             <Route exact path='/' component={Homepage}/>
             <Route path='/shop' component={ShopPage}/>
         </div>
