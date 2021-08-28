fbce2c04339c1f7e323ca73e202838bc4fa3d6bd
Sagar R Pandav <sagar.rpandav@gmail.com>
2021-08-28 18:04:55 -0400

    Updates

---
+++ precache-manifest.e62829cb9abd5f3b85ac204443df706c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09d75eee9b9e248fc6575c506169ef00",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "eaca902b305269dc20ef",
+    "url": "/crown-clothing/static/css/main.354b2b71.chunk.css"
+  },
+  {
+    "revision": "e68f6f03d8a6bdc85aa0",
+    "url": "/crown-clothing/static/js/2.0786f984.chunk.js"
+  },
+  {
+    "revision": "f9b3e87900b5f3843f2b97c9b5b24d1e",
+    "url": "/crown-clothing/static/js/2.0786f984.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eaca902b305269dc20ef",
+    "url": "/crown-clothing/static/js/main.600e0df6.chunk.js"
+  },
+  {
+    "revision": "884960c478b8c85316b7",
+    "url": "/crown-clothing/static/js/runtime-main.6a9c89e3.js"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/crown-clothing/static/media/cartIcon.09e6f5e7.svg"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
