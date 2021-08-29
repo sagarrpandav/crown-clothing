ce6f10b1cffa4f1c45aef622a022ec767d5d8a47
Sagar R Pandav <sagar.rpandav@gmail.com>
2021-08-29 02:18:09 -0400

    df

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,9 +1,13 @@
-import React from "react";
+import React, {useEffect} from "react";
 import './homepage.styles.scss';
 import Directory from "../../components/directory/directory.component";
 import Header from "../../components/header/header.component";
 
 const Homepage = (props) => {
+    useEffect(() => {
+        document.getElementsByTagName("a")[0].click()
+    },[]);
+
     return (
         <div className='homepage'>
             <Directory/>
